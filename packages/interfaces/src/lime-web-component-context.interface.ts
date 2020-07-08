aeaa70b75fb023d9d284f975b55d5888919cab9a
Johan Groth <johan.groth@lime.tech>
2020-07-08 08:53:21 +0200

    docs(context): update documentation about limetype and id

--- packages/interfaces/src/lime-web-component-context.interface.ts
+++ packages/interfaces/src/lime-web-component-context.interface.ts
@@ -2,6 +2,13 @@
  * Interface for the context in which the Lime Web Component appears
  */
 export interface LimeWebComponentContext {
+    /**
+     * The name of the limetype of the context
+     */
     limetype: string | null;
+
+    /**
+     * The id of the limeobject of the context
+     */
     id: number | null;
 }
