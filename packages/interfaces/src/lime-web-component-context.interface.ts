0ba530a09cbf5a7aa61e1e7f1046b584dad33dd2
Johan Groth <johan.groth@lime.tech>
2020-07-08 08:53:51 +0200

    feat(context): add support for nested contexts

--- packages/interfaces/src/lime-web-component-context.interface.ts
+++ packages/interfaces/src/lime-web-component-context.interface.ts
@@ -11,4 +11,9 @@ export interface LimeWebComponentContext {
      * The id of the limeobject of the context
      */
     id: number | null;
+
+    /**
+     * Optional parent context, if the current context is nested
+     */
+    parent?: LimeWebComponentContext;
 }
