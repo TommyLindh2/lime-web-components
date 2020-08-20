541d111fb0f7bc54929a6a001a41bf64f97a6f91
Johan Groth <johan.groth@lime.tech>
2020-08-20 16:33:16 +0200

    refactor: rename file

--- packages/decorators/src/factory.ts
+++ packages/decorators/src/factory.ts
@@ -8,15 +8,6 @@ import {
 import { getElement } from '@stencil/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 
-export * from './state/application-name';
-export * from './state/configs';
-export * from './state/current-user';
-export * from './state/device';
-export * from './state/filters';
-export * from './state/limeobjects';
-export * from './state/limetypes';
-export * from './state/session';
-
 export interface StateDecoratorConfig {
     /**
      * Name of the state service to use in the platform
