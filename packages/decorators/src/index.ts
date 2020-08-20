6fcdeee138c9b0019b87a1101aa7df0089184a9e
Johan Groth <johan.groth@lime.tech>
2020-08-20 16:34:55 +0200

    refactor: add index.ts again

---
+++ packages/decorators/src/index.ts
@@ -0,0 +1,9 @@
+export * from './state/application-name';
+export * from './state/configs';
+export * from './state/current-user';
+export * from './state/device';
+export * from './state/filters';
+export * from './state/limeobjects';
+export * from './state/limetypes';
+export * from './state/session';
+export { StateDecoratorConfig, createStateDecorator } from './factory';
