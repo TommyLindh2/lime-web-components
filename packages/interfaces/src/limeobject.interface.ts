5b689482db407bfc88be77c0a242f6e63a07f26c
Johan Groth <johan.groth@lime.tech>
2020-12-02 11:10:59 +0100

    feat(limeobject): deprecate getRelation in favor of getValue
    `getRelation` was a bit limited since it could only get the value of a relation and it was also not
    possible to get the value of nested properties. The new `getValue` method should fix this. It will
    allow any value on the object to be returned and will also allow nested values on related objects to
    be returned.
    
    re Lundalogik/crm-feature#1621

--- packages/interfaces/src/limeobject.interface.ts
+++ packages/interfaces/src/limeobject.interface.ts
@@ -28,8 +28,21 @@ export interface LimeObject {
      * Get related data of the object
      *
      * @param {Property} property the property of the related object
+     * @deprecated use `getValue` instead
      */
     getRelation(property: Property): LimeObject;
+
+    /**
+     * Get the value of a property by name
+     *
+     * @param {string} name name of property to get the value of. Allows a path
+     * to be specified using dot notation to get the value of a related
+     * property
+     *
+     * @returns {any} the value of the specified property. If it is a relation,
+     * the related `LimeObject` will be returned
+     */
+    getValue?(name: string): any;
 
     /**
      * Get the limetype of the object
