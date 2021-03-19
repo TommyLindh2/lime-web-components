da5ef718fa3ab760346e815345ff908d7487a377
Befkadu0 <befkassefa@gmail.com>
2021-03-19 13:56:22 +0100

    feat(filter-service): implement delete method to the filter service interface

--- packages/interfaces/src/service/state/filter-service.interface.ts
+++ packages/interfaces/src/service/state/filter-service.interface.ts
@@ -10,4 +10,12 @@ export interface FilterService extends StateService {
      * @returns {Promise<void>} a promise that will be resolved when the filter is saved
      */
     save(filter: Filter): Promise<void>;
+    /**
+     * Deletes the filter
+     *
+     * @param id a filter id to delete
+     *
+     * @returns {Promise<void>} a promise that will be resolved when the filter is deleted
+     */
+    delete?(filter: Filter): Promise<void>;
 }
